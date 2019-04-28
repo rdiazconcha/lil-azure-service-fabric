e5cc878fed4728ea938201e83762ff8dbc76bed1
booth06-mgr2 <booth06-mgr2@content.linkedin.biz>
2019-04-28 20:38:15 +0200

    Initial commit

---
+++ modulo-4/wcf/LinkedInLearning/Interfaces/IProductsService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IProductsService
+    {
+        Task<Product> GetProductAsync(string id);
+    }
+
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
