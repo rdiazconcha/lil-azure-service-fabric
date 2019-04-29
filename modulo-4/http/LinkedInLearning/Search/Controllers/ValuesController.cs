3181c874364a45bc2f424851428d4cd5e3fe8a4e
Rodrigo Díaz Concha <rodrigo.linkedin.learning@outlook.com>
2019-04-29 10:27:37 +0200

    Implemented the HTTP communication

--- modulo-4/http/LinkedInLearning/Search/Controllers/ValuesController.cs
+++ modulo-4/http/LinkedInLearning/Search/Controllers/ValuesController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Search.Models;
 
 namespace Search.Controllers
 {
@@ -11,9 +17,55 @@ namespace Search.Controllers
     public class ValuesController : ControllerBase
     {
         [HttpGet("{id}")]
-        public Task<ActionResult> Get(string id)
+        public async Task<ActionResult> Get(string id)
         {
-            throw new NotImplementedException();
+            var productsService = "fabric:/LinkedInLearning/Products";
+
+            var service = await ResolveAsync(productsService);
+
+            try
+            {
+                var client = new HttpClient();
+
+                var result = await client.GetAsync(service + $"/api/values/{id}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var content = await result.Content.ReadAsStringAsync();
+
+                    var product = JsonConvert.DeserializeObject<Product>(content);
+
+                    if (product != null)
+                    {
+                        return Ok(product);
+                    }
+
+                    return NotFound();
+
+                }
+
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+            
+        }
+
+        private async Task<string> ResolveAsync(string name)
+        {
+            var uri = new Uri(name);
+
+            var resolver = ServicePartitionResolver.GetDefault();
+
+            var service = await resolver.ResolveAsync(uri, ServicePartitionKey.Singleton, CancellationToken.None);
+
+            var addresses = JObject.Parse(service.GetEndpoint().Address);
+
+            var primary = (string)addresses["Endpoints"].First;
+
+            return primary;
         }
     }
 }
